05e1ca6ab5a0666621358840589c38586475b936
Ray Hung <ray.hung@onramplab.com>
2022-07-22 23:28:02 +0800

    feat: add todo list

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface TodoListProps {
+  items: { id: number, text: string }[]
+}
+
+const TodoList: React.FC<TodoListProps> = props => {
+  return <ul>{props.items.map(todo => <li>{todo.text}</li>)}</ul>;
+};
+
+export default TodoList;
